1736e97c4a65b4865a80eda1ee4a53ee82bcc707
GluePopper <148773969+GluePopper@users.noreply.github.com>
2026-09-12 09:19:11 +0700

    OpModeTemplate.java (1)

---
+++ OpModeTemplate.java
@@ -0,0 +1,17 @@
+@TeleOp(name = "TeleOpExample", group = "Linear Opmode")
+public class TeleOpExample extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        // Initialization
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Main robot loop
+
+        }
+    }
+}
